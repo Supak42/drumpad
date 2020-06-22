06098d74e14841311bca85d96f5c84730ce26175
User <User@DESKTOP-IC62OC3>
2020-06-22 22:49:40 +0700

    เพิ่ม ปุ่ม,ลำโพง

--- code/project/sketch_june21c/sketch_june21c.ino
+++ code/project/sketch_june21c/sketch_june21c.ino
@@ -3,6 +3,7 @@ const int ledPin = 13;
 int buttonState = 0;
 int count = 0;
 void setup() {
+  pinMode(9, OUTPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
 }
@@ -12,14 +13,28 @@ void loop() {
     count++;
   }
   switch (count) {
-  case 1: {
-    digitalWrite(ledPin, HIGH);
-  } break;
-  case 2: {
+  case 0: {
     digitalWrite(ledPin, LOW);
   } break;
-  case 3: {
-    count = 1;
+  case 1: {
+    digitalWrite(ledPin, HIGH);
+    beep(255);
+    beep(200);
+    beep(200);
+    beep(200);
+  } 
+  case 2: {
+    count = 0;
   } break;
   }
+}
+
+void beep(unsigned char delayms){
+
+analogWrite(9, 3); 
+delay(delayms);
+analogWrite(9, 0);
+delay(300); 
+
+
 }
